2cbbdb4d4d907f9cbdd8317a88a23e802a188cde
EriKorh <t1koer00@students.oamk.fi>
2022-12-23 12:11:43 +0200

    Kirjautuminen, rekisteröinti ja uloskirjautuminen backend

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+app.use("/api/likes", likeRoutes)
+
+app.listen(3001, () => {
+    console.log("API working");
+});
